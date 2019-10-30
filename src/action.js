60ebe371d3c5add01226a68afe8bb3f5ba0ec90b
gaolu11 <768065158@qq.com>
2019-10-30 15:02:03 +0800

    feat: 更新 action

--- src/action.js
+++ src/action.js
@@ -1,12 +1,11 @@
-import { SidePanelIdentifier } from './common/config'
 import { getSettingForKey } from './utils'
 
-export const onOpenDocument = context => {
-  console.error('✅✅✅ action onOpenDocument')
-}
-
-export const onSelectionChanged = context => {
+const autoSidePanelCommand = context => {
+  const objectID = (context.document || context.actionContext.document || MSDocument.currentDocument()).documentData().objectID()
+  const IdentifierPrefix = objectID ? `sketch-plugin-boilerplate-${objectID}` : 'sketch-plugin-boilerplate'
+  const SidePanelIdentifier = `${IdentifierPrefix}-side-panel`
   const isShow = getSettingForKey(SidePanelIdentifier)
+
   if (isShow) {
     const toggleSidePanelCommand = context.command.pluginBundle().commands()['sketch-plugin-boilerplate.toggle-side-panel']
     context.command = toggleSidePanelCommand
@@ -16,6 +15,14 @@ export const onSelectionChanged = context => {
       context
     )
   }
+}
+
+export const onOpenDocument = context => {
+  setTimeout(autoSidePanelCommand.bind(null, context), 100)
+}
+
+export const onSelectionChanged = () => {
+
 }
 
 export const onCloseDocument = () => {
